120e8822f94f234cdcea66e26578a281794f12f2
Cervator <cervator@gmail.com>
2018-02-02 17:31:14 -0500

    API fix for MovingBlocks/Terasology#3118 and tiny code quality tweaks

--- src/test/java/org/terasology/polyworld/TinyEnvironment.java
+++ src/test/java/org/terasology/polyworld/TinyEnvironment.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 MovingBlocks
+ * Copyright 2018 MovingBlocks
  *
- * Licensed under the Apache License, Version 2.0 (the "License"){ }
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.terasology.polyworld;
 
 import org.mockito.Matchers;
@@ -59,7 +58,7 @@ public final class TinyEnvironment {
     }
 
     private static void setupConfig(Context context) {
-        Config config = new Config();
+        Config config = new Config(context);
         context.put(Config.class, config);
     }
 
